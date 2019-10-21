ab42960cccac399aca6e9ea82081002cefd55752
gianthead97 <nikola_jankovic@tuta.io>
2019-10-22 00:57:37 +0200

    dodat layout i neke funkcionalnosti

--- funcs.js
+++ funcs.js
@@ -7,7 +7,7 @@ function drawTable() {
         var row_content = "";
 
         for (let j = 0; j < 4; j++)  {
-            row_content = row_content + ("<td id = '" + String(i*4 + j)  + "'>" + "</td>");
+            row_content = row_content + ("<td id = x" + String(i*4 + j)  + ">" + "</td>");
         }
         row.innerHTML = row_content;
         table.appendChild(row);
@@ -16,17 +16,62 @@ function drawTable() {
     
 }
 
-
-function foo() {
-    var pics = document.getElementsByTagName("img");
-    for (var index = 0; index < pics.length; index++) {
-           console.log(index);
-           pics[index].addEventListener("click", function(index) {console.log(index)});
+function findFirstFalse(matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (!matrix[i][j])
+                return [i, j];
+        }
     }
-    
-    
+
+    return [-1, -1];
 }
 
 
-drawTable()
-foo()
+function initListener(matrix) {
+    var pics = document.getElementsByTagName("img");
+    for (var index = 0; index < pics.length; index++) 
+           pics[index].addEventListener("click", function () {
+               var player = this.getAttribute("src");
+               var emptyFields = findFirstFalse(matrix); 
+               matrix[emptyFields[0]][emptyFields[1]] = true;
+               var query = "#x" + String(emptyFields[0] * 4 + emptyFields[1]);
+               var image = document.createElement("img");
+               image.setAttribute("src", player);
+               document.querySelector(query).appendChild(image);
+
+           },
+        false)
+}
+
+
+
+
+function startGame() {
+
+}
+
+function not_filled_row(i, matrix) {
+    matrix[i].some(elem => {return elem == false});
+}
+
+function play() {
+    var matrix = Array(6);
+    for (let i = 0; i < matrix.length; i++)
+        matrix[i] = Array(4).fill(false);
+
+    console.log(matrix)
+
+    initListener(matrix);
+    for (let i = 0; i < 6; i++) {
+        var j = 0;
+        while (not_filled_row(i, matrix)) {
+            
+        }
+    }
+}
+
+drawTable();
+
+startGame();
+play();
