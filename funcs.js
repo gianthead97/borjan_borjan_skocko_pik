979f8f30380cc8a5cf11d3502e535c07469be62d
gianthead97 <nikola_jankovic@tuta.io>
2019-10-27 12:51:40 +0100

    add some stuff

--- funcs.js
+++ funcs.js
@@ -18,6 +18,15 @@ function drawTable() {
     document.body.appendChild(table);
 }
 
+function drawResult(index = -1) {
+
+    var skeleton = document.createElement("div");
+    skeleton.setAttribute("id", "res");
+
+
+    document.body.appendChild(skeleton);
+}
+
 function findFirstFalse(matrix) {
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
